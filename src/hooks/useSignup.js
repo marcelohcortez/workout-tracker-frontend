35396ee0958226ad72645c58d362fd3d04f083fd
marcelohcortez <marcelohcortez@gmail.com>
2024-01-11 19:39:44 +0100

    Hardcode fecth server to test

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -12,7 +12,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch(`${serverURL}/api/user/signup`, {
+        const response = await fetch('https://workout-tracker-backend-r7sd.onrender.com/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
